e54787ead8e8788c55dbfe49143ff75f719bc2a0
Jorge Alberto Campos Ponce <poncealucard@gmail.com>
2025-06-25 17:41:17 -0600

    Update API URL in tipoRequisitoService
    Changed the API_URL from '/api' to '/tipos-requisito' to reflect the correct endpoint for tipoRequisitoService requests.

--- ProyectoFrontendDWI/src/services/tipoRequisitoService.js
+++ ProyectoFrontendDWI/src/services/tipoRequisitoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8001/api';
+const API_URL = 'http://localhost:8001/tipos-requisito';
 
 export const getCategorias = () =>
     axios.get(`${API_URL}/categorias`);
