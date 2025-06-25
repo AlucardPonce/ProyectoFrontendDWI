047611621f586a6976be8f659fe56bfa28e113e7
Jorge Alberto Campos Ponce <poncealucard@gmail.com>
2025-06-25 17:45:35 -0600

    Revert "Update API URL in tipoRequisitoService"
    This reverts commit e54787ead8e8788c55dbfe49143ff75f719bc2a0.

--- ProyectoFrontendDWI/src/services/tipoRequisitoService.js
+++ ProyectoFrontendDWI/src/services/tipoRequisitoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8001/tipos-requisito';
+const API_URL = 'http://localhost:8001/api';
 
 export const getCategorias = () =>
     axios.get(`${API_URL}/categorias`);
